d9f770ca52432ed670e9d9d6c64a7c52235410ea
Ely Karr <elykarr@gmail.com>
2021-12-15 17:19:32 -0800

    Passing core deliverables but not entirely as expected

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant }) {
+  const [inStock, setInStock] = useState(true);
+
+  function handleInStock(event) {
+    setInStock(!inStock)
+  }
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant ? plant.image : "https://via.placeholder.com/400"} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      {inStock ? (
+        <button onClick={handleInStock} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
-      )}
+          <button onClick={handleInStock}>Out of Stock</button>
+        )}
     </li>
   );
 }
