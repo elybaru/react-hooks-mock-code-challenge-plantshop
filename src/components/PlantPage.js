142cce34667a417194df977b68fa7d48a95cc0d6
Ely Karr <elykarr@gmail.com>
2021-12-15 17:19:11 -0800

    commit tree 12b8051cef7645eef252d17bbc928e87eebab422

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((resp) => setPlants(resp))
+  }, [])
+
+  let plantsToDisplay;
+
+  function filterPlants(searchTerm) {
+    plantsToDisplay = plants.filter((plant) => plant.name.includes(searchTerm) || searchTerm === "")
+    setPlants(plantsToDisplay);
+
+  }
+
+  // Not behaving the way I expected, doesn't rerender when search is cleared, case sensitive, can't get toLowerCase() to work for me
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm plants={plants} setPlants={setPlants} />
+      <Search filterPlants={filterPlants} />
+      <PlantList plants={plants} />
     </main>
   );
 }
